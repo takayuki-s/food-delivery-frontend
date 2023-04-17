60f2067c959a5dd9eed5a560d1085f976c84ada5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-04-17 09:18:35 +0900

    レストラン検索input作成

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
-import Head from 'next/head'
-import { Alert, Button } from 'reactstrap'
+import { Col, Input, InputGroup, InputGroupText, Row } from 'reactstrap'
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <Alert color="primary">Hello Project</Alert>
-        <Button color="primary">Hello from nextjs</Button>
-      </div>
+    <div className="container-fluid">
+      <Row>
+        <Col>
+          <div className="search">
+            <InputGroup>
+              <InputGroupText>探す</InputGroupText>
+              <Input placeholder="レストラン名を入力してください" />
+            </InputGroup>
+          </div>
+        </Col>
+      </Row>
+      <style jsx>{`
+        .search {
+          margin: 20px;
+          width: 500px;
+        }
+      `}</style>
     </div>
   )
 }
